633fc899380f1377a0910b8809005766d5078bf4
Travis Cline <travis.cline@gmail.com>
2013-10-23 12:49:09 -0500

    Refactor SecretService provider a bit.
    Remove unneeded build flag

--- keyring_linux.go
+++ keyring_linux.go
@@ -1,4 +1,4 @@
-// +build linux
+// +build !gnome_keyring
 
 package keyring
 
@@ -8,38 +8,38 @@ import (
 )
 
 const (
-	ss_ServiceName    = "org.freedesktop.secrets"
-	ss_ServicePath    = "/org/freedesktop/secrets"
-	ss_CollectionPath = "/org/freedesktop/secrets/collection/default"
-
-	ss_ServiceIface    = "org.freedesktop.Secret.Service."
-	ss_SessionIface    = "org.freedesktop.Secret.Session."
-	ss_CollectionIface = "org.freedesktop.Secret.Collection."
-	ss_ItemIface       = "org.freedesktop.Secret.Item."
-	ss_PromptIface     = "org.freedesktop.Secret.Prompt."
+	ssServiceName    = "org.freedesktop.secrets"
+	ssServicePath    = "/org/freedesktop/secrets"
+	ssCollectionPath = "/org/freedesktop/secrets/collection/default"
+	ssServiceIface    = "org.freedesktop.Secret.Service."
+	ssSessionIface    = "org.freedesktop.Secret.Session."
+	ssCollectionIface = "org.freedesktop.Secret.Collection."
+	ssItemIface       = "org.freedesktop.Secret.Item."
+	ssPromptIface     = "org.freedesktop.Secret.Prompt."
 )
 
-// The .Item interface speaks these. Note: Order is important
-type ss_Secret struct {
+// ssSecret corresponds to org.freedesktop.Secret.Item
+// Note: Order is important
+type ssSecret struct {
 	Session      dbus.ObjectPath
 	Parameters   []byte
 	Value        []byte
-	Content_type string
+	ContentType string `dbus:"Content_type"`
 }
 
-// We'll always use text/plain, may need tweaking if implementing encryption
-// other than "plain"
-func new_ss_Secret(session dbus.ObjectPath, secret string) (s ss_Secret) {
-	s = ss_Secret{
-		Content_type: "text/plain; charset=utf8",
-		Parameters:   []byte(""),
+// newSSSecret prepares an ssSecret for use
+// Uses text/plain as the Content-type which may need to change in the future
+func newSSSecret(session dbus.ObjectPath, secret string) (s ssSecret) {
+	s = ssSecret{
+		ContentType: "text/plain; charset=utf8",
+		Parameters:   []byte{},
 		Session:      session,
 		Value:        []byte(secret),
 	}
 	return
 }
 
-// Currently hard-coded to use the 'default' keychain
+// SsProvider implements the provider interface freedesktop SecretService
 type SsProvider struct {
 	*dbus.Conn
 	srv *dbus.Object
@@ -50,31 +50,31 @@ type SsProvider struct {
 func (s *SsProvider) openSession() (*dbus.Object, error) {
 	var disregard dbus.Variant
 	var sessionPath dbus.ObjectPath
-	path := fmt.Sprint(ss_ServiceIface, "OpenSession")
+	path := fmt.Sprint(ssServiceIface, "OpenSession")
 	err := s.srv.Call(path, 0, "plain", dbus.MakeVariant("")).Store(&disregard, &sessionPath)
 	if err != nil {
 		return nil, err
 	}
-	return s.Object(ss_ServiceName, sessionPath), nil
+	return s.Object(ssServiceName, sessionPath), nil
 }
 
 // Unsure how the .Prompt call surfaces, it hasn't come up.
 func (s *SsProvider) unlock(p dbus.ObjectPath) error {
 	var unlocked []dbus.ObjectPath
 	var prompt dbus.ObjectPath
-	err := s.srv.Call(fmt.Sprint(ss_ServiceIface, "Unlock"), 0, []dbus.ObjectPath{p}).Store(&unlocked, &prompt)
+	err := s.srv.Call(fmt.Sprint(ssServiceIface, "Unlock"), 0, []dbus.ObjectPath{p}).Store(&unlocked, &prompt)
 	if err != nil {
 		return err
 	}
 	if prompt != dbus.ObjectPath("/") {
-		s.Object(ss_ServiceName, prompt).Call(fmt.Sprint(ss_PromptIface, "Prompt"), 0, "unlock")
+		s.Object(ssServiceName, prompt).Call(fmt.Sprint(ssPromptIface, "Prompt"), 0, "unlock")
 	}
 	return nil
 }
 
 func (s *SsProvider) Get(c, u string) (string, error) {
 	var unlocked, locked []dbus.ObjectPath
-	var secret ss_Secret
+	var secret ssSecret
 	search := map[string]string{
 		"username": u,
 		"service":  c,
@@ -84,10 +84,10 @@ func (s *SsProvider) Get(c, u string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	s.unlock(ss_CollectionPath)
-	collection := s.Object(ss_ServiceName, ss_CollectionPath)
+	s.unlock(ssCollectionPath)
+	collection := s.Object(ssServiceName, ssCollectionPath)
 
-	collection.Call(fmt.Sprint(ss_CollectionIface, "SearchItems"), 0, search).Store(&unlocked, &locked)
+	collection.Call(fmt.Sprint(ssCollectionIface, "SearchItems"), 0, search).Store(&unlocked, &locked)
 	// results is a slice. Just grab the first one.
 	if len(unlocked) == 0 && len(locked) == 0 {
 		return "", ErrNotFound
@@ -95,17 +95,17 @@ func (s *SsProvider) Get(c, u string) (string, error) {
 	if len(unlocked) == 0 {
 		for _, r := range locked {
 			s.unlock(r)
-			s.Object(ss_ServiceName, r).Call(fmt.Sprint(ss_ItemIface, "GetSecret"), 0, session.Path()).Store(&secret)
+			s.Object(ssServiceName, r).Call(fmt.Sprint(ssItemIface, "GetSecret"), 0, session.Path()).Store(&secret)
 			break
 		}
 	} else {
 		for _, r := range unlocked {
-			s.Object(ss_ServiceName, r).Call(fmt.Sprint(ss_ItemIface, "GetSecret"), 0, session.Path()).Store(&secret)
+			s.Object(ssServiceName, r).Call(fmt.Sprint(ssItemIface, "GetSecret"), 0, session.Path()).Store(&secret)
 			break
 		}
 	}
 
-	session.Call(fmt.Sprint(ss_SessionIface, "Close"), 0)
+	session.Call(fmt.Sprint(ssSessionIface, "Close"), 0)
 	return string(secret.Value), nil
 }
 
@@ -123,26 +123,26 @@ func (s *SsProvider) Set(c, u, p string) error {
 	if err != nil {
 		return err
 	}
-	s.unlock(ss_CollectionPath)
-	collection := s.Object(ss_ServiceName, ss_CollectionPath)
+	s.unlock(ssCollectionPath)
+	collection := s.Object(ssServiceName, ssCollectionPath)
 
-	secret := new_ss_Secret(session.Path(), p)
+	secret := newSSSecret(session.Path(), p)
 	// the bool is "replace"
-	collection.Call(fmt.Sprint(ss_CollectionIface, "CreateItem"), 0, properties, secret, true).Store(&item, &prompt)
+	collection.Call(fmt.Sprint(ssCollectionIface, "CreateItem"), 0, properties, secret, true).Store(&item, &prompt)
 	if prompt != "/" {
-		s.Object(ss_ServiceName, prompt).Call(fmt.Sprint(ss_PromptIface, "Prompt"), 0, "unlock")
+		s.Object(ssServiceName, prompt).Call(fmt.Sprint(ssPromptIface, "Prompt"), 0, "unlock")
 	}
-	session.Call(fmt.Sprint(ss_SessionIface, "Close"), 0)
+	session.Call(fmt.Sprint(ssSessionIface, "Close"), 0)
 	return nil
 }
 
 func init() {
 	conn, err := dbus.SessionBus()
 	if err != nil {
-		fmt.Println("Error connecting to bus, bailing")
+		fmt.Println("keyring/dbus: Error connecting to dbus session, not registering SecretService provider")
 		return
 	}
-	srv := conn.Object(ss_ServiceName, ss_ServicePath)
+	srv := conn.Object(ssServiceName, ssServicePath)
 	p := &SsProvider{conn, srv}
 
 	// Everything should implement dbus peer, so ping to make sure we have an object...
