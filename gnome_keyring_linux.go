767e138e9e8772e8cae1ae122343349d2dc4fa89
Yauhen Lazurkin <yauhenl@gojuno.com>
2017-03-17 11:58:33 +0300

    Use libsecret instead deprecated libgnome-keyring

--- gnome_keyring_linux.go
+++ gnome_keyring_linux.go
@@ -3,33 +3,41 @@
 package keyring
 
 /*
-#cgo pkg-config: gnome-keyring-1 glib-2.0
+#cgo pkg-config: libsecret-1 glib-2.0
 #include <stdlib.h>
-#include "gnome-keyring.h"
+#include "libsecret/secret.h"
 
-GnomeKeyringPasswordSchema keyring_schema =
-  { GNOME_KEYRING_ITEM_GENERIC_SECRET,
-    { { "username", GNOME_KEYRING_ATTRIBUTE_TYPE_STRING },
-      { "service",  GNOME_KEYRING_ATTRIBUTE_TYPE_STRING },
-      { NULL,      0 } } };
+SecretSchema keyring_schema =
+  {
+    "org.github.tmc.keyring.Password",
+    SECRET_SCHEMA_NONE,
+    {
+      { "username", SECRET_SCHEMA_ATTRIBUTE_STRING },
+      { "service",  SECRET_SCHEMA_ATTRIBUTE_STRING },
+      {  NULL, 0 },
+    }
+  };
 
 // wrap the gnome calls because cgo can't deal with vararg functions
 
-GnomeKeyringResult gkr_set_password(gchar *description, gchar *service, gchar *username, gchar *password) {
-	return gnome_keyring_store_password_sync(
+gboolean gkr_set_password(gchar *description, gchar *service, gchar *username, gchar *password, GError **err) {
+	return secret_password_store_sync(
 		&keyring_schema,
 		NULL,
 		description,
 		password,
+    NULL,
+    err,
 		"service", service,
 		"username", username,
 		NULL);
 }
 
-GnomeKeyringResult gkr_get_password(gchar *service, gchar *username, char **password) {
-	return gnome_keyring_find_password_sync(
+gchar * gkr_get_password(gchar *service, gchar *username, GError **err) {
+	return secret_password_lookup_sync(
 		&keyring_schema,
-		(gchar**)password,
+    NULL,
+		err,
 		"service", service,
 		"username", username,
 		NULL);
@@ -38,16 +46,9 @@ GnomeKeyringResult gkr_get_password(gchar *service, gchar *username, char **pass
 import "C"
 
 import (
-	"errors"
 	"fmt"
 	"unsafe"
 )
-
-var errorMap = map[C.GnomeKeyringResult]error{
-	C.GNOME_KEYRING_RESULT_IO_ERROR:  errors.New("Error communicating with the gnome-keyring daemon"),
-	C.GNOME_KEYRING_RESULT_CANCELLED: errors.New("Operation was cancelled"),
-	C.GNOME_KEYRING_RESULT_NO_MATCH:  ErrNotFound,
-}
 
 type gnomeKeyring struct{}
 
@@ -61,34 +62,33 @@ func (p gnomeKeyring) Set(Service, Username, Password string) error {
 	defer C.free(unsafe.Pointer(service))
 	defer C.free(unsafe.Pointer(password))
 
-	result := C.gkr_set_password(desc, service, username, password)
+	var gerr *C.GError
+	result := C.gkr_set_password(desc, service, username, password, &gerr)
+	defer C.free(unsafe.Pointer(gerr))
 
-	if result != C.GNOME_KEYRING_RESULT_OK {
-		if knownErr, ok := errorMap[result]; ok {
-			return knownErr
-		}
-		return fmt.Errorf("Unknown gnome-keyring error: %d", int(result))
+	if result == 0 {
+		return fmt.Errorf("Gnome-keyring error: %+v", gerr)
 	}
 	return nil
 }
 
 func (p gnomeKeyring) Get(Service string, Username string) (string, error) {
-	var pw *C.char
+	var gerr *C.GError
+	var pw *C.gchar
+
 	username := (*C.gchar)(C.CString(Username))
 	service := (*C.gchar)(C.CString(Service))
 	defer C.free(unsafe.Pointer(username))
 	defer C.free(unsafe.Pointer(service))
 
-	result := C.gkr_get_password(service, username, &pw)
-	defer C.gnome_keyring_free_password((*C.gchar)(pw))
+	pw = C.gkr_get_password(service, username, &gerr)
+	defer C.free(unsafe.Pointer(gerr))
+	defer C.secret_password_free((*C.gchar)(pw))
 
-	if result != C.GNOME_KEYRING_RESULT_OK {
-		if err, ok := errorMap[result]; ok {
-			return "", err
-		}
-		return "", fmt.Errorf("Unknown gnome-keyring error: %d", int(result))
+	if pw == nil {
+		return "", fmt.Errorf("Gnome-keyring error: %+v", gerr)
 	}
-	return C.GoString(pw), nil
+	return C.GoString((*C.char)(pw)), nil
 }
 
 func init() {
